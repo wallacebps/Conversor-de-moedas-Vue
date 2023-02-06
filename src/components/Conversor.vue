7df776b4a999124499bbfe5d8a7757dd4740a149
wallacebps <wallacebps@hotmail.com>
2023-02-05 21:02:55 -0300

    conversor created

---
+++ src/components/Conversor.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="conversor">
+        <h2>{{ moedaA }} para {{ moedaB }}</h2>
+        <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
+        <input type="button" value="Converter">
+        <h2>{{ moedaB_value }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Conversor",
+    props: ['moedaA', 'moedaB'],
+    data(){
+        return{
+            moedaA_value: "",
+            moedaB_value: 0
+        }
+    },
+    methods: {
+        converter(){
+            
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-import './assets/main.css'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
